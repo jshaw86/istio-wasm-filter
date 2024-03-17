c8df8fd529afe3733782fa8522a0cf14a9c82e8a
Jordan Shaw <jordan.shaw@textnow.com>
2024-03-17 02:20:49 -0500

    reqwest and source address integration
    Signed-off-by: Jordan Shaw <jordan.shaw@textnow.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 use log::info;
 use proxy_wasm as wasm;
+use proxy_wasm::traits::Context;
+use std::str;
 
 #[no_mangle]
 pub fn _start() {
@@ -18,9 +20,30 @@ struct HelloWorld {
 impl wasm::traits::Context for HelloWorld {}
 
 impl wasm::traits::HttpContext for HelloWorld {
-    fn on_http_response_headers(&mut self, num_headers: usize, _end_of_stream: bool) -> wasm::types::Action {
-        info!("Got {} HTTP headers in #{}.", num_headers, self.context_id);
-        self.set_http_response_header("x-hello", Some("v0.4"));
+
+    fn on_http_request_headers(&mut self, _num_headers: usize, _end_of_stream: bool) -> wasm::types::Action {
+        let headers = self.get_http_request_headers();
+
+        for (name, value) in &headers {
+            info!("name {} value {}", name, value)
+        } 
+
+        wasm::types::Action::Continue
+    }
+
+
+    fn on_http_response_headers(&mut self, _num_headers: usize, _end_of_stream: bool) -> wasm::types::Action {
+        let prop = self.get_property(["source", "address"].to_vec()).unwrap();
+        let addr = match str::from_utf8(&prop) {
+            Ok(v) => v,
+            Err(_e) => "",
+        };
+       
+        //let resp = reqwest::blocking::get("http://eu.httpbin.org/ip")?
+        //      .json::<HashMap<String, String>>()?;
+
+        self.set_http_response_header("x-hello", Some(&format!("Hello world from v0.5 {}", self.context_id)));
+        self.set_http_response_header("x-ip", Some(&addr));
 
         wasm::types::Action::Continue
     }
